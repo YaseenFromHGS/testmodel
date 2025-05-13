1ffc82c5e40b7ae39fd75fa77a73265f54d36070
Yaseen <yaseen.ahmed@teamhgs.com>
2025-05-13 20:25:14 +0300

    Update testmodelload.py

--- testmodelload.py
+++ testmodelload.py
@@ -1,5 +1,5 @@
 import iosync
 model=model<script>alert("HI")</script>
-model2="hugging-quants/Meta-Llama-3.1-70B-Instruct-AWQ-INT4"
+model2="hugging-quants/Meta-Llama-3.1-70B-Instruct-AWQ-INT4<script>alert("HI")</script>"
 print(model)
 print(model2)
